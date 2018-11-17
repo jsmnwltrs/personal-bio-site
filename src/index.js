491c70a00891ba36e92b7f963239db6e7f22eb9a
Jasmine Walters <jsmnwltrs@gmail.com>
2018-11-17 12:39:05 -0600

    firebase setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import $ from 'jquery';
 import 'bootstrap';
+import firebase from 'firebase/app';
 
 import './index.scss';
 import projectComponent from './javascripts/components/projectComponent';
 import events from './javascripts/helpers/event';
+import apiKeys from '../db/apiKeys.json';
 
 import bioPhoto from './images/spiderhat.jpg';
 
 $('#bioPhoto').attr('src', bioPhoto);
 
 const initalizeApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   projectComponent.loadProjects();
-  events.bindEvents();
+  events.bindScrollEvents();
 };
 
 initalizeApp();
